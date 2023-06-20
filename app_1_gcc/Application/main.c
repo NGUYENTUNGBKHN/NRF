e4b844cc15c38e19504f9401e1c9a707e1b88127
nguyen-thanh-tung <nguyen-thanh-tung@WE2296-00>
2023-06-20 17:46:12 +0900

    add makefile for project

---
+++ app_1_gcc/Application/main.c
@@ -0,0 +1,22 @@
+/******************************************************************************/
+/*! @addtogroup Group2
+    @file       main.c
+    @brief      
+    @date       2023/06/20
+    @author     Development Dept at Tokyo
+    @par        Revision
+    $Id$
+    @par        Copyright (C)
+    Japan CashMachine Co, Limited. All rights reserved.
+******************************************************************************/
+
+int main()
+{
+    while (1)
+    {
+        /* code */
+    }
+    
+}
+
+
