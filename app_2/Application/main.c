4881681210d350182c19cc5c3828aed19881cd66
nguyen-thanh-tung <nguyen-thanh-tung@WE2296-00>
2023-06-29 13:51:19 +0900

    add uart drver

--- app_2/Application/main.c
+++ app_2/Application/main.c
@@ -9,6 +9,10 @@
     @par        Copyright (C)
     Japan CashMachine Co, Limited. All rights reserved.
 ******************************************************************************/
+#include "main.h"
+
+// create driver uart instance
+static nrf_drv_uart_t uart_instance = NRF_DRV_UART_INSTANCE(APP_UART_DRIVER_INSTANCE);
 
 int main()
 {
@@ -20,5 +24,23 @@ int main()
     }
     return 0;
 }
+
+
+void uart_init()
+{
+    ret_code_t err;
+    nrf_drv_uart_config_t uart_config;
+
+    uart_config.baudrate = ;
+    uart_config.hwfc = ;
+    uart_config.p_context = ;
+    uart_config.parity = ;
+
+    err = nrf_drv_uart_init(uart_instance, uart_config);
+
+
+}
+
+
 
 
---
+++ app_2/Application/main.h
@@ -0,0 +1,26 @@
+/******************************************************************************/
+/*! @addtogroup Group2
+    @file       main.h
+    @brief      
+    @date       2023/06/28
+    @author     Development Dept at Tokyo
+    @par        Revision
+    $Id$
+    @par        Copyright (C)
+    Japan CashMachine Co, Limited. All rights reserved.
+******************************************************************************/
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* CODE */
+#include "nrf_drv_uart.h"
+
+#ifdef __cplusplus
+}
+#endif
+#endif
