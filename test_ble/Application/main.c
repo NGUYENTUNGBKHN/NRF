ba6bec1f5f107cc3943eb5c24ffc5b96b3e70856
nguyen-thanh-tung <nguyenthanhtung8196@gmail.com>
2023-08-17 18:14:35 +0900

    add test ble

---
+++ test_ble/Application/main.c
@@ -0,0 +1,24 @@
+/******************************************************************************/
+/*! @addtogroup Group2
+    @file       main.c
+    @brief      
+    @date       2023/08/17
+    @author     Development Dept at Tokyo
+    @par        Revision
+    $Id$
+    @par        Copyright (C)
+    Japan CashMachine Co, Limited. All rights reserved.
+******************************************************************************/
+#include <stdio.h>
+
+void SystemInit()
+{
+
+}
+
+int main()
+{
+
+    while(1);
+}
+
