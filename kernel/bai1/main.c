19897e1d211773a24f0d5c1b082c524af91030f8
nguyen-thanh-tung <nguyenthanhtung8196@gmail.com>
2023-08-30 18:55:27 +0900

    blink led

--- kernel/bai1/main.c
+++ kernel/bai1/main.c
@@ -2,16 +2,20 @@
 #include "nrf.h"             // Device header
 #include "system_nrf52840.h" // NordicSemiconductor::Device:Startup
 
-#define CNF_POS_A_DIR   0
+#define CNF_POS_A_DIR 0
 #define CNF_POS_B_INPUT 1
-#define CNF_POS_C_PULL  2
+#define CNF_POS_C_PULL 2
 #define CNF_POS_D_DRIVE 8
 
 NRF_GPIO_Type *gpio = NRF_P0;
 
+void sysTick(int time)
+{
+}
+
 void timer_init()
 {
-	NRF_TIMER_Type *timer0 = NRF_TIMER0;
+    NRF_TIMER_Type *timer0 = NRF_TIMER0;
 
     timer0->PRESCALER = 4;
     timer0->MODE = 0;
@@ -19,43 +23,49 @@ void timer_init()
     timer0->CC[0] = 1000;
     timer0->INTENSET = 1 << 16;
     timer0->SHORTS = 1;
-    
+
     NVIC_EnableIRQ(TIMER0_IRQn);
     timer0->TASKS_START = 1;
 }
 
 void gpio_on()
 {
-    gpio->OUT |= 0 << 13; 
+    gpio->OUT &= ~(1 << 13);
 }
 
 void gpio_off()
 {
-    gpio->OUT |= 1 << 13; 
+    gpio->OUT |= 1 << 13;
 }
 
 void gpio_init()
 {
     gpio->PIN_CNF[13] = (uint32_t)((1 << CNF_POS_A_DIR) |
-                         (1 << CNF_POS_B_INPUT) |
-                         (0 << CNF_POS_C_PULL) |
-                         (0 << CNF_POS_D_DRIVE));
+                                   (1 << CNF_POS_B_INPUT) |
+                                   (0 << CNF_POS_C_PULL) |
+                                   (0 << CNF_POS_D_DRIVE));
 }
 
 void TIMER0_IRQHandler()
 {
     static int count = 1;
+    static uint8_t onof = 1;
     if (NRF_TIMER0->EVENTS_COMPARE[0] == 1)
     {
         NRF_TIMER0->EVENTS_COMPARE[0] = 0;
         count++;
-        if (count % 2 == 0)
+        if (count % 1000 == 0)
         {
-            gpio_on();
-        }
-        else
-        {
-            gpio_off();
+            if (onof)
+            {
+                onof = 0;
+                gpio_off();
+            }
+            else
+            {
+                onof = 1;
+                gpio_on();
+            }
         }
     }
 }
@@ -65,11 +75,8 @@ int main()
 
     gpio_init();
     timer_init();
-    
 
     // gpio->DIR |= 1 << 13;
-    
-    
 
     while (1)
     {
