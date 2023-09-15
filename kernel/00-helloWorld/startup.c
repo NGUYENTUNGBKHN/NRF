7e56e3bd8505443f9b2c1a8d6cbb5d78ecedfc88
nguyen-thanh-tung <nguyen-thanh-tung@WE2296-00>
2023-09-15 13:37:27 +0900

    fix BIN vs HEX

--- kernel/00-helloWorld/startup.c
+++ kernel/00-helloWorld/startup.c
@@ -1,12 +1,12 @@
 #include <stdint.h>
 
 extern void main();
-void Reset_Handler()
+void Reset_Handler(void)
 {
     main();
 }
 
-__attribute((section("isr_vector")))
+__attribute((section(".isr_vector")))
 uint32_t *isr_vectors[] = {
     0,
     (uint32_t*)Reset_Handler,
